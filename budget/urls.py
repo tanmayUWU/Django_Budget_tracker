34b6744ee432b7d06aa70d550ae82d3730c56e88
TanmayUWU <tpaddalwar19@gmail.com>
2026-02-19 00:31:55 +0530

    thinner views, multiple urls try-1

--- budget/urls.py
+++ budget/urls.py
@@ -4,4 +4,14 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    
+    path("expense/delete/<int:delete_id>/", views.delete_expense, name="delete_expense"),
+    path("income/delete/<int:delete_id>/", views.delete_income, name="delete_income"),
+    
+    path("expense/add/", views.add_expense, name="add_expense"),
+    path("income/add/", views.add_income, name="add_income"),
+    
+    path("expense/edit/<int:edit_id>/", views.edit_expense, name="edit_expense"),
+    path("income/edit/<int:edit_id>/", views.edit_income, name="edit_income"),
+    
 ]
--- budget/views.py
+++ budget/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Income, Expense
 from .forms import ExpenseForm,IncomeForm
 from django.utils import timezone
@@ -56,72 +56,129 @@ def index(request):
         expenses = list_of_expences()
         curr_bal = current_balance()
         this_m_spending = this_month_spending()
-        form = ExpenseForm()
-        form_income = IncomeForm()
+
         passing_dict = {
             "incomes":incomes,
             "expenses":expenses,
             "curr_bal":curr_bal,
-            "this_m_spending":this_m_spending,
-            "form":form,
-            "form_income":form_income
-           
-            # "catg_comp":catg_comp
-            
+            "this_m_spending":this_m_spending
+                       
         }
-        if request.method == 'POST':
-            if 'save' in request.POST:
-                pk = request.POST.get('save')
-                if not pk:
-                    form = ExpenseForm(request.POST)
-                else:
-                    record = Expense.objects.get(id=pk)
-                    form = ExpenseForm(request.POST,instance=record)
-                form.save()
-                return redirect(reverse('index'))
+        # if request.method == 'POST':
+        #     if 'save' in request.POST:
+        #         pk = request.POST.get('save')
+        #         if not pk:
+        #             form = ExpenseForm(request.POST)
+        #         else:
+        #             record = Expense.objects.get(id=pk)
+        #             form = ExpenseForm(request.POST,instance=record)
+        #         form.save()
+        #         return redirect(reverse('index'))
                 
-            if 'save_Income' in request.POST:
-                pk = request.POST.get('save_Income')
-                if not pk:
-                    form_income = IncomeForm(request.POST)
-                else:
-                    record = Income.objects.get(id=pk)
-                    form_income = IncomeForm(request.POST,instance=record)
-                form_income.save()
-                return redirect(reverse('index'))
+        #     if 'save_Income' in request.POST:
+        #         pk = request.POST.get('save_Income')
+        #         if not pk:
+        #             form_income = IncomeForm(request.POST)
+        #         else:
+        #             record = Income.objects.get(id=pk)
+        #             form_income = IncomeForm(request.POST,instance=record)
+        #         form_income.save()
+        #         return redirect(reverse('index'))
             
-            if 'delete' in request.POST:
-                pk = request.POST.get('delete')
-                record = Expense.objects.get(id=pk)
-                record.delete()
+        #     if 'delete' in request.POST:
+        #         pk = request.POST.get('delete')
+        #         record = Expense.objects.get(id=pk)
+        #         record.delete()
                 
-            if 'delete_income' in request.POST:
-                pk = request.POST.get('delete_income')
-                record = Income.objects.get(id=pk)
-                record.delete()
+        #     if 'delete_income' in request.POST:
+        #         pk = request.POST.get('delete_income')
+        #         record = Income.objects.get(id=pk)
+        #         record.delete()
                 
-            if 'edit_income' in request.POST:
-                pk = request.POST.get('edit_income')
-                record = Income.objects.get(id=pk)
-                form_income = IncomeForm(instance=record)
-                # return redirect(reverse('index'))
+        #     if 'edit_income' in request.POST:
+        #         pk = request.POST.get('edit_income')
+        #         record = Income.objects.get(id=pk)
+        #         form_income = IncomeForm(instance=record)
                 
-            if 'edit_expense' in request.POST:
-                pk = request.POST.get('edit_expense')
-                record = Expense.objects.get(id=pk)
-                form = ExpenseForm(instance=record)
-                # return redirect(reverse('index'))
-                
-                
-        # passing_dict['incomes'] = incomes
-        # passing_dict['expenses'] = expenses
-        passing_dict['form'] = form
-        passing_dict['form_income'] = form_income
+        #     if 'edit_expense' in request.POST:
+        #         pk = request.POST.get('edit_expense')
+        #         record = Expense.objects.get(id=pk)
+        #         form = ExpenseForm(instance=record)
+
+        # passing_dict['form'] = form
+        # passing_dict['form_income'] = form_income
             
         
                 
     except Exception as e:
         raise (f"Ran into an exception")
     return render(request, "budget/index.html", passing_dict)
+
+
+def add_expense(request):
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        # Checing if form is valid and checks all the validations
+        if form.is_valid():
+            
+            form.save()
+            return redirect('index')
+    else:
+        form = ExpenseForm()
+    # Returning an empty form after recieving an invalid form
+    return render(request, "budget/expense_form.html",{'form':form})
+
+
+def add_income(request):
+    if request.method == 'POST':
+        form_income = IncomeForm(request.POST)
+        if form_income.is_valid():
+
+            form_income.save()
+            return redirect('index')
+        
+    else:
+        form_income = IncomeForm()
+            
+    return render(request, "budget/income_form.html",{'form_income':form_income})
+
+def edit_income(request, edit_id):
+    record = get_object_or_404(Income,id = edit_id)
+    if request.method == 'POST':
+        form_income = IncomeForm(request.POST, instance=record)
+        if form_income.is_valid():
+            form_income.save()
+            return redirect('index')
+    else:
+        form_income = IncomeForm(instance=record)
+    return render(request, "budget/income_form.html",{'form_income':form_income})    
+
+
+def edit_expense(request,edit_id):
+    record = get_object_or_404(Expense,id=edit_id)
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST, instance=record)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        
+    else:
+        form = ExpenseForm(instance=record)
+    return render(request,"budget/expense_form.html", {'form':form})
+
+
+def delete_expense(request,delete_id):
+
+    if request.method == 'POST':
+        record = get_object_or_404(Expense,id=delete_id)
+        record.delete()
+    return redirect('index')
+    
+    
+def delete_income(request, delete_id):
+    if request.method == 'POST':
+        record = get_object_or_404(Income,id=delete_id)
+        record.delete()
+    return redirect('index')
 
 
